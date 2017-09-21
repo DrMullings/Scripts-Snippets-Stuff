d3555e4ce8496d2afb979606193a673616ce496e
Joachim Rauch <jora@pinky.arch.suse.de>
2017-09-21 14:00:46 +0200

    Add set PATH to initBash
    initBash now also sets the PATH variable for the scripts

--- initBash.sh
+++ initBash.sh
@@ -10,3 +10,7 @@ echo 'alias ll="ls -l "' >> $ALIASFILE
 echo 'alias la="ls -a "' >> $ALIASFILE
 echo 'alias lla="ls -la "' >> $ALIASFILE
 echo 'alias sudo="sudo "' >> $ALIASFILE
+
+echo "Setting path"
+echo "export PATH=$PATH:/home/$USER/Scripts" >> /home/$USER/.bashrc
+
