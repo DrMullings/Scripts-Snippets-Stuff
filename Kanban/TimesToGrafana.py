2295932b548aea51c3d2672e4cb6f76486d2a5b1
Joachim Rauch <jrauch@suse.com>
2020-08-18 15:20:14 +0200

    Fixed Typo, added Typecast
    Pushed values are now being casted to float to avoid datatype issues
    with influxdb

--- Kanban/TimesToGrafana.py
+++ Kanban/TimesToGrafana.py
@@ -4,11 +4,13 @@
 from redminelib import Redmine
 import pdb
 import datetime
+import time
 import logging
 import json
 import statistics
 import configparser
 from influxdb import InfluxDBClient
+import sys
 
 config = configparser.ConfigParser()
 config.read('config_do_never_push.ini')
@@ -22,14 +24,24 @@ INFLUX_USER = config['INFLUX']['User']
 INFLUX_PASS = config['INFLUX']['Pass']
 INFLUX_DATABASE = config['INFLUX']['Database']
 
-redmine = Redmine(REDMINE_URL, key=REDMINE_KEY)
-influx = InfluxDBClient(host=INFLUX_HOST, port=INFLUX_PORT, username=INFLUX_USER, password=INFLUX_PASS, database=INFLUX_DATABASE)
+try:
+    redmine = Redmine(REDMINE_URL, key=REDMINE_KEY)
+    influx = InfluxDBClient(host=INFLUX_HOST, port=INFLUX_PORT, username=INFLUX_USER, password=INFLUX_PASS, database=INFLUX_DATABASE)
+except Exception as e:
+    print("Exception when connecting to Databases ",e)
+    sys.exit(1)
 
 prjlist = ['suseqa', 'openqav3', 'openqatests']
 taglist = [ 'epic', 'saga', 'fate']
-cycleList = []
-leadList = []
+points = []
 
+time = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+#create result dictionary
+results = {}
+for prj in prjlist:
+    results[prj] = {}
+ 
 id_codes = {
         'New' : '1',
         'In Progress' : '2',
@@ -50,60 +62,59 @@ def is_story(tckt):
         return False
     return True
 
-for prj in prjlist:
-    tickets = redmine.issue.filter(project_id=prj, status_id=id_codes["Resolved"])
-
-    for tckt in tickets:
-        # only consider normal [u] tickets
-        if not is_story(tckt):
-            continue
-
-        crtDate = tckt.created_on
-        endDate = tckt.closed_on
-        # set an invalid default
-        prgDate = None
-
-        journals = tckt.journals
-        for jrn in journals:
-            try:
-                # look at first entry to not get an array
-                jDetails = json.loads(json.dumps(jrn.details[0]))
-            except:
-                # empty details
-                continue
-            if went_in_progress(jDetails):
-                prgDate = jrn.created_on
-                #print("Went in progress: " + str(prgDate))
-                break
-
-        leadTime = endDate - crtDate
-        leadList.append(leadTime.days)
-
-        if prgDate:
-            cycTime = endDate - prgDate
-            cycleList.append(cycTime.days)
-
-meanCycTime = statistics.mean(cycleList)
-medianCycTime = statistics.median(cycleList)
-
-meanLeadTime = statistics.mean(leadList)
-medianLeadTime = statistics.median(leadList)
-
-points = [
-            {
-                "measurement": "Times",
-                "time": datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
-                "fields": {
-                    "MeanCycleTime": meanCycTime,
-                    "MedianCycleTime": medianCycTime,
-                    "MeanLeadTime": meanLeadTime,
-                    "MediaLeadTime": medianLeadTime
+def create_point(prj, leadTime, cycleTime):
+    point = {
+        "measurement": prj,
+        "time": time,
+        "fields": {
+            "Cycletime": cycleTime, 
+            "Leadtime": leadTime
                 }
             }
-        ]
+    return point
 
+try:
+    for prj in prjlist:
+        tickets = redmine.issue.filter(project_id=prj, status_id=id_codes["Resolved"])
+
+        for tckt in tickets:
+            if not is_story(tckt):
+                continue
+
+            crtDate = tckt.created_on
+            endDate = tckt.closed_on
+            # set an invalid default
+            prgDate = None
+
+            journals = tckt.journals
+            for jrn in journals:
+                try:
+                    # look at first entry to not get an array
+                    jDetails = json.loads(json.dumps(jrn.details[0]))
+                except:
+                    # empty details
+                    continue
+                if went_in_progress(jDetails):
+                    prgDate = jrn.created_on
+                    #print("Went in progress: " + str(prgDate))
+                    break
+
+            leadTime = endDate - crtDate
+            leadTime = leadTime.days
+
+            if prgDate:
+                cycTime = endDate - prgDate
+                cycTime = cycTime.days
+            else:
+                cycTime = None
+
+            points.append(create_point(prj, leadTime, cycTime))
+
+except Exception as e:
+    print("Exception during parsing ", e)
+    sys.exit(2)
 
 #push to influxdb
 
-influx.write_points(points, time_precision='ms', database=INFLUX_DATABASE, retention_policy=None, tags=None, batch_size=None, protocol=u'json', consistency=None)
+influx.write_points(points, time_precision='s', database=INFLUX_DATABASE, retention_policy=None, tags=None, batch_size=None, protocol=u'json', consistency=None)
 
